Fix temperature scaling training with LBFGS, forward with softmax, change the seed, ECE y_true assignment bug

main.py
@@ -70,7 +70,7 @@ from src.evaluation import (
     evaluate_confidences,
 )
 from src.plots import plot_conf, plot_uncert, plot_reliability_diagram
-
+# from pudb import set_trace; set_trace() # for debugging
 
 # SECRETS
 SECRET_IMPORTED = False
@@ -271,12 +271,6 @@ def run_calibration_benchmark(
     # Do scaling methods for calibration
     temperature_scalers = {}
     for method in ["ps_seq_likelihood", "ts_seq_likelihood"]:
-         for question_data in calibration_data[split].values():
-               try:
-                    print(question_data["seq_likelihood"])
-               except TypeError:
-                    print("TypeError: ", question_data)
-                    continue
 
          train_likelihoods = np.array(
                [
@@ -319,7 +313,7 @@ def run_calibration_benchmark(
                    scaler.train_parameters(
                         train_probabilities=train_likelihoods,
                         train_targets=train_targets,
-                        batch_size=calibration_batch_size,
+                        batch_size=DATASET_SPLIT_SIZES[dataset_name]["train"], # use fullbatch for L-BFGS 
                         learning_rate=calibration_learning_rate,
                         num_steps=calibration_num_steps,
                    )
@@ -349,12 +343,12 @@ def run_calibration_benchmark(
 
               cot_likelihoods = np.array(
                    [
-                        question_data["cot_likelihood"]
+                        question_data["cot_seq_likelihood"]
                         for question_data in split_data.values()
                    ]
               )
               cot_likelihoods[np.isnan(cot_likelihoods)] = 0
-              baseline_confidences[split_name]["cot_likelihood"] = cot_likelihoods
+              baseline_confidences[split_name]["cot_seq_likelihood"] = cot_likelihoods
 
               # Parametric scaling methods
               if method in ["ps_seq_likelihood", "ts_seq_likelihood"]:
@@ -435,14 +429,14 @@ def run_calibration_benchmark(
                         correctness,
                         save_path=os.path.join(
                              img_dir,
-                             f"{split_name}_{baseline_name}.pdf",
+                             f"{split_name}_{baseline_name}_{dataset_name}_{model_name.replace("/", "_")}.pdf",
                         ),
                         success_percentage=baseline_results.get(
                              f"{split_name}_{baseline_name}_success", 1
                         ),
                    )
 
-    # Save results
+    # Create results directory to save results
     baseline_results_dir=None
     
     if result_dir is not None:
@@ -458,7 +452,10 @@ def run_calibration_benchmark(
 
     for baseline_name, baseline_data in eval_data.items():
          with open(
-              os.path.join(baseline_results_dir, f"{timestamp}_{baseline_name}_results.dill"), 
+              os.path.join(
+                   baseline_results_dir, 
+                   f"{timestamp}_{baseline_name}_results.dill",
+               ), 
               "wb",
          ) as results_file:
               dill.dump(
@@ -474,7 +471,6 @@ def run_calibration_benchmark(
                    results_file,
               )
     results_df = pd.DataFrame(columns=EVAL_METRIC_ORDER)
-
     # CREATE RESULTS DATAFRAME
     test_splits = [
          split for split in calibration_data.keys() if "test" in split

methods/platt_scaling/platt_scaling.py
@@ -9,7 +9,7 @@ import warnings
 # EXTERNAL LIB
 import numpy as np
 import pandas as pd
-import tqdm
+from tqdm import tqdm # minds the module for IMPORTING
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -20,7 +20,7 @@ from src.data import loop_dataloader
 class PlattScaling(nn.Module):
     def __init__(self): # checkout the name __init__ for underscore NUMBER
         super().__init__()
-        self.scale = nn.Parameter(torch.ones(1) * 1.5) # prevent extreme values
+        self.scale = nn.Parameter(torch.ones(1) * 0.5) # prevent extreme values
         self.bias = nn.Parameter(torch.zeros(1))
     
     def forward(self, raw_probabilities: torch.FloatTensor) -> torch.FloatTensor:

methods/temperature_scaling/temp_scaling.py
@@ -14,7 +14,7 @@ import warnings
 # EXTERNAL LIB
 import numpy as np
 import pandas as pd
-import tqdm
+from tqdm import tqdm # mind the modules for importing tqdm
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -28,7 +28,7 @@ class TemperatureScaling(nn.Module):
         self.temperature = nn.Parameter(torch.ones(1) * 1.5)
     
     def forward(self, raw_probabilities: torch.FloatTensor) -> torch.FloatTensor:
-        return F.softmax(raw_probabilities / self.temperature, dim=1)
+        return F.softmax(raw_probabilities / self.temperature, dim=-1) # can be negative
     
     def train_parameters(
         self,
@@ -38,36 +38,27 @@ class TemperatureScaling(nn.Module):
         learning_rate: float,
         num_steps: int,
     ):
-        optimizer = optim.lbfgs([self.temperature], lr=learning_rate)
-        loss_fn = nn.CrossEntropyLoss()
+        optimizer = optim.LBFGS([self.temperature], lr=learning_rate, max_iter=num_steps)
+        loss_fn = nn.NLLLoss()
         train_dataloader = DataLoader(
-            TensorDataset(train_probabilities, train_targets),
+            TensorDataset(train_probabilities, train_targets.type(torch.LongTensor)),
             batch_size=batch_size,
         )
-        best_temperature = self.temperature.clone().detach()
-        best_loss = float("inf")
         with tqdm(total=num_steps) as pbar:
-            for i, (inputs, targets) in tqdm(
-                enumerate(loop_dataloader(train_dataloader)), total=num_steps
-            ):
-                if i > num_steps:
+            for i, (inputs, targets) in enumerate(loop_dataloader(train_dataloader)): 
+                if i >= 1:
                     break
 
-                outputs = self.forward(inputs)
-                loss = loss_fn(outputs, targets)
-                loss.backward()
-                optimizer.step()   
-                optimizer.zero_grad()
-                pbar.set_description(
-                    f"Step #{i+1} - Loss: {loss.detach().cpu().item():.4f}"
-                )                
-                pbar.update(1)
-        
-                if loss.item() < best_loss:
-                    best_loss = loss.item()
-                    best_temperature = self.temperature.clone().detach()
+                def closure(): # necessary for LBFGS optimizer   
+                    optimizer.zero_grad()
+                    outputs = self.forward(inputs)
+                    log_outputs = torch.log(outputs)
+                    loss = loss_fn(log_outputs, targets)
+                    loss.backward()
+                    return loss
+                optimizer.step(closure)
         
         with torch.no_grad():
-            self.temperature = self.temperature.copy_(best_temperature) # inplace update
-        print(f"Training for Temperature Scaling completed. Best training Loss: {best_loss:.4f}")
+            self.temperature.clamp_(min=1e-6) # make sure temperature is positive
+        print(f"Training for Temperature Scaling completed. Best temperature: {self.temperature.item():.4f}")
 

src/data.py
@@ -10,6 +10,7 @@ from typing import Dict, Any, Callable, List, Tuple, Optional
 import dill
 from httpx import HTTPStatusError
 from openai import OpenAI
+import openai
 # External lib
 import tiktoken
 import datasets
@@ -124,6 +125,10 @@ def num_tokens_from_messages(messages, model="gpt-4o-mini-2024-07-18"):
     num_tokens += 3  # every reply is primed with <|start|>assistant<|message|>
     return num_tokens
 
+########################################################################
+###### GENERATE CALIBRATION OUTPUTS USING THE PREPROCESSED INPUTS ######
+########################################################################
+
 def extract_black_box_calibration_data(
     model_name: str,
     num_in_context_samples: int,
@@ -164,7 +169,6 @@ def extract_black_box_calibration_data(
 
     else:
         split_calibration_data = {}
-
 
         with open(
             os.path.join(source_data_dir, f"calibration_data_{split}.dill"), "rb"
@@ -297,6 +301,8 @@ def extract_black_box_calibration_data(
         calibration_split_path = os.path.join(
             calibration_data_dir, f"calibration_data_{split}.dill"
         )
+    except openai.AuthenticationError as e:
+        print(f"API Connection Error: {e}")
     finally:
 
         if len(open_ai_calibration_data) > 0:
@@ -406,18 +412,18 @@ def extract_model_calibration_data(
         # generate sequence likelihoods
         generated_answer_ids = outputs["sequences"][
             :, inputs.shape[1] :
-        ].squeeze(0)
+        ].squeeze(0) # extract only output sequence ids, starting from the input length to the end.
         predictions = torch.log(
             F.softmax(torch.stack(
                 outputs["scores"], dim=1), 
                 dim=-1
-            ) # convert logits to probabilities
+            ) # Apply softmax to the last dim (vocabulary size)
         )
         log_probs = torch.gather(
             predictions,
             dim=-1,
             index=generated_answer_ids.unsqueeze(-1),
-        ).squeeze(-1) # get the log probability of the generated answer
+        ).squeeze(-1) # extract the log probability of the generated answer [sequence_length_generated, 1]
         token_mask = torch.all(
             torch.stack(
                 [
@@ -427,10 +433,10 @@ def extract_model_calibration_data(
                 dim=-1,
             ),
             dim=-1,
-        ).long() # remove the special tokens
-        num_tokens = token_mask.sum(dim=-1) # count the number of tokens
-        seq_likelihoods = (log_probs * token_mask).sum(-1) / num_tokens # calculate the average log probability
-        seq_likelihoods = torch.exp(seq_likelihoods) # convert to probability exponential
+        ).long() # this line converts bool tensor to long tensor (1s and 0s)
+        num_tokens = token_mask.sum(dim=-1) # count the number of tokens, Since token_mask is a 1D tensor, dim=-1 sums over the entire tensor.
+        seq_likelihoods = (log_probs * token_mask).sum(-1) / num_tokens # calculate the average log probability, Averaging allows for comparison across sequences of different lengths.
+        seq_likelihoods = torch.exp(seq_likelihoods) # convert average lop probs to average pertoken  probability exponential
         #CoT
         cot_generated_answer_ids = cot_outputs["sequences"][
             :, cot_inputs.shape[1] :
@@ -604,7 +610,10 @@ def create_or_load_calibration_data(
         del calibration_data["included_questions"]
     
     return calibration_data, included_questions
-        
+
+########################################
+######GENERATE INPUTS###################
+########################################
 # Create a preprocess_batch_wrapper for TruthfulQA dataset
 def preprocess_batch_wrapper(
     train_data: Dataset,
@@ -647,12 +656,13 @@ def preprocess_batch_wrapper(
                 
             few_shot_prompts.append(few_shot_prompt)
         
+        # Qwen adaptation for English
         batch_with_prompt = [
-            few_shot_prompt + "Question: " + question + " Answer: "
+            few_shot_prompt + "Question: " + question + " Your response should only be in english and no other language." + " Answer: "
             for question, few_shot_prompt in zip(batch["question"], few_shot_prompts)
         ]
         batch_with_cot_prompt = [
-            few_shot_prompt + QA_COT_PROMPT + " Question: " + question + " Answer: "
+            few_shot_prompt + QA_COT_PROMPT + " Question: " + question + " Your response should only be in english and no other language." + " Answer: "
             for question, few_shot_prompt in zip(batch["question"], few_shot_prompts)
         ]
         tokenizer.padding_side = "left" # necessary for left padding
@@ -731,11 +741,11 @@ def preprocess_batch_wrapper_truthful_qa(
             few_shot_prompts.append(few_shot_prompt)
         
         batch_with_prompt = [
-            few_shot_prompt + "Question: " + question + " Answer: "
+            few_shot_prompt + "Question: " + question + " Your response should only be in english and no other language." + " Answer:"
             for question, few_shot_prompt in zip(batch["question"], few_shot_prompts)
         ]
         batch_with_cot_prompt = [
-            few_shot_prompt + QA_COT_PROMPT + " Question: " + question + " Answer: "
+            few_shot_prompt + QA_COT_PROMPT + " Question: " + question + " Your response should only be in english and no other language." + " Answer:"
             for question, few_shot_prompt in zip(batch["question"], few_shot_prompts)
         ]
         tokenizer.padding_side = "left" # necessary for left padding
@@ -927,7 +937,7 @@ def preprocess_truthful_qa(
         for split_name, data_split, data_loader_path in zip(
             ["train", "test"], 
             [train_data, test_data], 
-            [train_data_loader_path, test_data_loader_path]
+            [train_data_loader_path, test_data_loader_path],
         ):
         
             if os.path.exists(data_loader_path):

src/evaluation.py
@@ -20,7 +20,7 @@ from sklearn.isotonic import IsotonicRegression # isotonic regression
 def check_answer_correctness(
     correct_answers: List[str],
     model_answers: List[str],
-    bleu_threshold: float = 0.5, # threshold for BLEU score
+    bleu_threshold: float = 0.3, # threshold for BLEU score
 ) -> List[bool]:
     global bleu_score
 
@@ -97,10 +97,10 @@ def evaluate_confidences(
         infix = f"{add_name}_"
         
     metrics = {
-        f"{split_name}_{infix}ECE": ece(all_confidences, all_correctness),
+        f"{split_name}_{infix}ECE": ece(y_true=all_correctness, y_pred=all_correctness),
         f"{split_name}_{infix}SmoothECE": SmECE(f=np.array(all_confidences), y=np.array(all_targets)),
         f"{split_name}_{infix}Brier_Score": brier_score_loss(
-            y_true=all_confidences, y_prob=all_correctness
+            y_true=all_correctness, y_prob=all_confidences
         ),
         f"{split_name}_{infix}AUROC": roc_auc_score(
             y_true=all_correctness, y_score=all_confidences,